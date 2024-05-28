1493710b0929a86af7c182394d36dc7e2473f086
dengkx <dkx@t-chip.com.cn>
2024-05-28 19:20:39 +0800

    rk356x: aio-3568j-amp-ubuntu.mk

---
+++ rk356x/aio-3568j-amp-ubuntu.mk
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+source $CUR_DIR/firefly-rk3568-ubuntu.mk
+
+export RK_UBOOT_DEFCONFIG=rk3568
+# Kernel defconfig fragment
+export RK_KERNEL_DEFCONFIG_FRAGMENT="$RK_KERNEL_DEFCONFIG_FRAGMENT rk356x_amp.config"
+# Kernel dts
+export RK_KERNEL_DTS=rk3568-firefly-aioj-amp
+# PRODUCT MODEL
+export RK_PRODUCT_MODEL=AIO_3568J
+
